cfedc985b9dcb44bb694ea1aaff9a9f15c590ea3
Daniel Cueto <dcueto14.info@gmail.com>
2023-10-16 15:59:44 +0200

    Init Project

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+import Header from "./components/Header.svelte";
+
+</script>
+
+<Header />
+
+
+<style>
+
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logo from '../assets/filmin_logo.png';
+</script>
+
+<header>
+  <img src={logo} />
+  <nav>
+    <ul>
+      <li><a href="#">Menu 1</a></li>
+      <li><a href="#">Menu 1</a></li>
+      <li><a href="#">Menu 1</a></li>
+      <li><a href="#">Menu 1</a></li>
+    </ul>
+  </nav>
+</header>
+
+
+<style>
+  header{
+    display: flex;
+  }
+</style>
